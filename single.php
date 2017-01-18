bb11739c02b53a77c1d01a70d7d9646f325a6578
Daniel J. Schneider <dan@scrollwright.com>
2017-01-17 23:14:45 -0700

    First round of tweaks

--- single.php
+++ single.php
@@ -10,9 +10,14 @@
 							<div class="postdate">Posted by <strong><?php the_author() ?></strong> on  <?php the_time('F jS, Y') ?> <?php if (current_user_can('edit_post', $post->ID)) { ?> | <?php edit_post_link('Edit', '', ''); } ?></div>
 			
 							<div class="entry">
-                                <?php if ( function_exists('has_post_thumbnail') && has_post_thumbnail() ) { the_post_thumbnail( 'full', array('class' => 'alignleft post_thumbnail')); } ?>
-								<?php the_content('Read more &raquo;'); ?>
-								<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+								<?php if(function_exists('social_warfare')):
+								    social_warfare();
+								endif;
+								the_content('Read more &raquo;');
+								wp_link_pages(array('before' => '<div class="page-links"><strong>Pages: </strong>', 'after' => '</div>', 'link_before' => '<span class="page-link-number">', 'link_after' => '</span>', 'next_or_number' => 'number'));
+								if(function_exists('social_warfare')):
+								    social_warfare();
+								endif; ?>
 							</div>
 							<div class="postmeta">This post is <?php if(get_the_tags()) { the_tags('', ', '); } ?> in <?php the_category(', ') ?></div>
 						
@@ -25,13 +30,11 @@
 						</div><!--/post-<?php the_ID(); ?>-->
 
 						</div>
-
-				<?php comments_template(); ?>
 				
 				<?php endwhile; ?>
 			
 				<?php endif; ?>
-			</div>
+				</div>
 			</div>
 		<?php get_sidebars(); ?>
 	</div>
